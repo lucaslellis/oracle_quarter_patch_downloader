f826a48c108d159a755b85060ab0cbf0b471669b
Lucas Pimentel Lellis <lucaslellis@gmail.com>
2022-05-31 12:30:17 -0300

    Avoid downloading existing files Fixes #1

--- oraclepatchdownloader.py
+++ oraclepatchdownloader.py
@@ -8,10 +8,12 @@ Requires:
     - html5lib
 """
 
+import hashlib
 import os
 import pathlib
 import re
 from http import HTTPStatus
+import sys
 
 import requests
 from bs4 import BeautifulSoup
@@ -23,6 +25,7 @@ _HEADERS = {"User-Agent": "Wget/1.20.3"}
 _DEFAULT_HTML_PARSER = "html5lib"
 
 _CHUNK_SIZE = 2097152  # 2 MB
+
 
 class OraclePatchDownloader:
     """Class that enables downloading Oracle patches
@@ -44,7 +47,6 @@ class OraclePatchDownloader:
         self.__download_links = None
         self.username = username
         self.password = password
-
 
     def download_oracle_patch(
         self,
@@ -74,21 +76,30 @@ class OraclePatchDownloader:
             self.__logon_oracle_support()
 
         if self.__platform_codes is None:
-            self.__build_list_platform_codes(
-                platform_names
-            )
+            self.__build_list_platform_codes(platform_names)
 
         pathlib.Path(target_dir).mkdir(parents=True, exist_ok=True)
 
         if self.__download_links:
             self.__download_links.clear()
 
-        self.__build_list_download_links(
-            patch_number
-        )
+        self.__build_list_download_links(patch_number)
 
         for dl_link in self.__download_links:
-            self.__download_link(dl_link, target_dir, progress_function)
+            try:
+                self.__download_link(dl_link, target_dir, progress_function)
+            except ChecksumMismatch:
+                local_filename = (
+                    target_dir
+                    + os.path.sep
+                    + self.__extract_file_name_from_url(dl_link)
+                )
+                print(
+                    f"{local_filename}"
+                    " checksum does not match Oracle's checksum. "
+                    "Please remove it manually and download it again.",
+                    file=sys.stderr,
+                )
 
     def __logon_oracle_support(
         self,
@@ -226,6 +237,7 @@ class OraclePatchDownloader:
                     - (int): file size in bytes
                     - (int): total downloaded in bytes
         """
+        oracle_file_checksum = self.__obtain_sha256_checksum_oracle(url)
 
         file_name = self.__extract_file_name_from_url(url)
 
@@ -238,18 +250,27 @@ class OraclePatchDownloader:
         else:
             file_size = int(file_size)
 
-        total_dl = 0
-        with open(
-            target_dir + os.path.sep + file_name,
-            "wb",
-        ) as dl_file:
-            for chunk in resp_dl.iter_content(_CHUNK_SIZE):
-                total_dl += len(chunk)
-                dl_file.write(chunk)
-                if file_size and progress_function:
-                    progress_function(file_name, file_size, total_dl)
+        if self.__check_file_exists(target_dir, file_name, file_size):
+            progress_function(file_name, file_size, file_size)
+        else:
+            total_dl = 0
+            with open(
+                target_dir + os.path.sep + file_name,
+                "wb",
+            ) as dl_file:
+                for chunk in resp_dl.iter_content(_CHUNK_SIZE):
+                    total_dl += len(chunk)
+                    dl_file.write(chunk)
+                    if file_size and progress_function:
+                        progress_function(file_name, file_size, total_dl)
 
-    def __extract_file_name_from_url(self, url):
+        downloaded_file_checksum = self.__calculate_file_checksum(
+            target_dir, file_name
+        )
+        if oracle_file_checksum != downloaded_file_checksum:
+            raise ChecksumMismatch
+
+    def __extract_file_name_from_url(self, url) -> str:
         """Extracts the file name from url.
 
         Args:
@@ -265,3 +286,75 @@ class OraclePatchDownloader:
         file_name = re.sub("[?].+$", "", file_name)
 
         return file_name
+
+    def __check_file_exists(self, target_dir, file_name, file_size) -> bool:
+        """Check if a file exists and has the correct size.
+
+        Args:
+            target_dir (str): The target directory to download the file.
+            file_name (str): Name of the file being downloaded.
+            file_size (_type_): Size in bytes of the original file.
+        """
+        target_file = pathlib.Path(target_dir + os.path.sep + file_name)
+        if target_file.is_file():
+            if target_file.stat().st_size == file_size:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def __obtain_sha256_checksum_oracle(self, url) -> str:
+        """Obtains the SHA-256 checksum from Oracle for a patch file.
+
+        Args:
+            url (str): the link to be downloaded
+
+        Returns:
+            str: SHA-256 for the file on Oracle Support
+        """
+        checksum = ""
+        aru_matches = re.search("[?]aru=[0-9]+", url)
+        if aru_matches:
+            aru = aru_matches.group(0).split("=")[1]
+            resp_chksum = requests.get(
+                "https://updates.oracle.com/Orion/ViewDigest/get_form",
+                params={"aru": aru},
+                cookies=self.__cookie_jar,
+                headers=_HEADERS,
+            )
+            if resp_chksum.text:
+                sha256_matches = re.search(
+                    r"\b[A-Fa-f0-9]{64}\b", resp_chksum.text
+                )
+                if sha256_matches:
+                    checksum = sha256_matches.group(0)
+
+        return checksum.upper()
+
+    def __calculate_file_checksum(self, target_dir, file_name) -> str:
+        """Calculates the SHA-256 checksum of the downloaded file.
+
+        Args:
+            target_dir (str): _description_
+            file_name (str): _description_
+
+        Returns:
+            str: SHA-256 checksum of the downloaded file
+        """
+        hash_chunk_size = 128 * 1024
+        with open(target_dir + os.path.sep + file_name, "rb") as checked_file:
+            file_hash = hashlib.sha256()
+            file_chunk = checked_file.read(hash_chunk_size)
+            while file_chunk:
+                file_hash.update(file_chunk)
+                file_chunk = checked_file.read(hash_chunk_size)
+
+        if file_hash:
+            return file_hash.hexdigest().upper()
+        else:
+            return ""
+
+
+class ChecksumMismatch(Exception):
+    """Raised when the downloaded file checksum does not match Oracle's."""
